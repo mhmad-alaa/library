c7fd1c0425ccccdc87f999bb24c583d9609a5da4
mhmad-alaa <ma2001129@gmail.com>
2023-08-07 01:28:56 +0300

    add popup form buttons functionality

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-// const header = document.getElementById('header');
-// const open = document.getElementById('add-btn');
-// const footer = document.getElementById('footer');
-
+const header = document.getElementById("header");
+const open = document.getElementById("add-btn");
+const footer = document.getElementById("footer");
+const popup = document.getElementById("popup");
 
 function popupBook() {
-    document.getElementById('popup').classList.add("popup-book"); 
+  document.getElementById("popup").classList.add("popup-book");
 }
 
-function closePoput() {
-    document.getElementById('body').classList.remove("popup-book"); 
+function closePopup() {
+  document.getElementById("popup").classList.remove("popup-book");
+}
+
+function submitNewBook() {
+  document.getElementById("popup").classList.remove("popup-book");
 }
