2c28469260f1c15fc3574d4ce94679f9da009d85
mhmad-alaa <ma2001129@gmail.com>
2023-08-10 04:03:01 +0300

    feat: add intial grid layout to added book cards

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-
-
-window.addEventListener("DOMContentLoaded", function() {
+window.addEventListener("DOMContentLoaded", function () {
   const submit = document.querySelector("#submit");
 
-  submit.addEventListener("click", function() {
+  submit.addEventListener("click", function () {
     newBookCard();
   });
 });
-
 
 function resetForm() {
   document.getElementById("title").value = "";
@@ -16,7 +13,6 @@ function resetForm() {
   document.getElementById("read-it-box").checked = false;
 }
 
-
 function formValues() {
   const title = document.querySelector("#title").value;
   const author = document.getElementById("author").value;
@@ -24,7 +20,7 @@ function formValues() {
   const readIt = document.getElementById("read-it-box").checked;
 
   console.log(title, author, pages, readIt);
-  closePopup(); 
+  closePopup();
 
   return { title, author, pages, readIt };
 }
@@ -41,8 +37,8 @@ function closePopup() {
 }
 
 function checkReadBtn(isRead, readBtn) {
-  readBtn.textContent = (!Book.readIt? "Read" : "Not read");
-  readBtn.style.backgroundColor = (!Book.readIt? '#9fff9c' : '#ff9c9c');
+  readBtn.textContent = !Book.readIt ? "Read" : "Not read";
+  readBtn.style.backgroundColor = !Book.readIt ? "#9fff9c" : "#ff9c9c";
 }
 
 function newBookCard() {
@@ -50,47 +46,41 @@ function newBookCard() {
   if (!Book.title || !Book.author || !Book.pages) {
     alert("Un completed data!");
   } else {
-    const bookCardContainer = document.createElement('div');
-    const bookCard = document.createElement('div');
-    const title = document.createElement('div');
-    const author = document.createElement('div');
-    const pages = document.createElement('div');
-    const readBtn = document.createElement('button');
-    const removeBtn = document.createElement('button');
-
+    const bookCard = document.createElement("div");
+    const title = document.createElement("div");
+    const author = document.createElement("div");
+    const pages = document.createElement("div");
+    const readBtn = document.createElement("button");
+    const removeBtn = document.createElement("button");
 
     title.textContent = `"${Book.title}"`;
     author.textContent = Book.author;
     pages.textContent = `${Book.pages} pages`;
-    readBtn.textContent = (Book.readIt? "Read" : "Not read");
-    readBtn.style.backgroundColor = (Book.readIt? '#9fff9c' : '#ff9c9c');
+    readBtn.textContent = Book.readIt ? "Read" : "Not read";
+    readBtn.style.backgroundColor = Book.readIt ? "#9fff9c" : "#ff9c9c";
     removeBtn.textContent = "Remove";
-    
+
     // to do => do read-status and remove buttons onclick
     // readBtn.onclick = checkReadBtn(Book.readIt, readBtn);
 
-
-    bookCardContainer.classList.add('book-cards-container');
-    bookCard.classList.add('books-cards');
-    title.classList.add('book-card-title');
-    author.classList.add('book-card-author');
-    pages.classList.add('book-cnt-pages');
-    readBtn.classList.add('book-read');
-    removeBtn.classList.add('book-remove');
+    bookCard.classList.add("books-cards");
+    title.classList.add("book-card-title");
+    author.classList.add("book-card-author");
+    pages.classList.add("book-cnt-pages");
+    readBtn.classList.add("book-read");
+    removeBtn.classList.add("book-remove");
 
     bookCard.appendChild(title);
     bookCard.appendChild(author);
     bookCard.appendChild(pages);
     bookCard.appendChild(readBtn);
     bookCard.appendChild(removeBtn);
-    bookCardContainer.appendChild(bookCard);
-    
-    document.querySelector('body').appendChild(bookCardContainer); 
 
-    console.log(bookCardContainer); 
+    document.getElementById("booksGrid").appendChild(bookCard);
+
+    // console.log(bookCard);
 
     document.getElementById("popup").classList.remove("active");
     document.getElementById("overlay").classList.remove("active");
   }
 }
-
